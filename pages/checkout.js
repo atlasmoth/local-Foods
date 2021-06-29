5ace682b51752457b0ac8cca0e220d1f8f84d890
atlasmoth <ebuka422@gmail.com>
2021-06-29 13:52:29 +0100

    got rid of use-shopping-cart

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,6 +1,4 @@
 import Layout from "./../components/layout";
-import { useShoppingCart } from "use-shopping-cart";
-import { useEffect } from "react/cjs/react.development";
 
 export default function Checkout() {
   // console.log(process.env.NEXT_PUBLIC_PUBLIC_STRIPE);
@@ -15,54 +13,5 @@ export default function Checkout() {
 }
 
 function Elements() {
-  const productData = [
-    {
-      name: "Bananas",
-      id: "some_unique_id_1",
-      price: 400,
-      image: "https://www.fillmurray.com/300/300",
-      currency: "USD",
-      product_data: {
-        metadata: {
-          type: "fruit",
-        },
-      },
-      price_data: {
-        recurring: {
-          interval: "week",
-        },
-      },
-    },
-    {
-      name: "Tangerines",
-      id: "some_unique_id_2",
-      price: 100,
-      image: "https://www.fillmurray.com/300/300",
-      currency: "USD",
-      product_data: {
-        metadata: {
-          type: "fruit",
-        },
-      },
-      price_data: {
-        recurring: {
-          interval: "week",
-        },
-      },
-    },
-  ];
-  const { totalPrice, redirectToCheckout, cartCount, loadCart } =
-    useShoppingCart();
-  useEffect(() => {
-    loadCart(productData);
-  }, []);
-  return (
-    <div>
-      <p>Number of Items: {cartCount}</p>
-      <p>Total: {totalPrice}</p>
-
-      {/* Redirects the user to Stripe */}
-      <button onClick={() => redirectToCheckout()}>Checkout</button>
-    </div>
-  );
+  return <div></div>;
 }
