2ce6740c10abc738822f24f5d5f7b40814af618b
atlasmoth <ebuka422@gmail.com>
2021-06-29 22:54:21 +0100

    done for today

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Splash from "../components/splash";
+import Cookie from "next-cookies";
 
 export default function Home() {
   return (
@@ -6,4 +7,18 @@ export default function Home() {
       <Splash />
     </div>
   );
+}
+export async function getServerSideProps(context) {
+  const res = context.res;
+  const req = context.req;
+  const { foodsUser } = Cookie(context);
+  if (foodsUser) {
+    res.writeHead(302, { Location: `/app` });
+    res.end();
+    return { props: {} };
+  } else {
+    res.writeHead(302, { Location: `/login` });
+    res.end();
+    return { props: {} };
+  }
 }
