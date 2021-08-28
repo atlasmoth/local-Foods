0e9ee5e899f64544157847f6d23492708cfc1572
atlasmoth <ebuka422@gmail.com>
2021-08-28 21:06:48 +0100

    patching mongo db code

--- pages/api/search.js
+++ pages/api/search.js
@@ -7,17 +7,42 @@ async function Search(req, res) {
     const { db } = await connectToDatabase();
     const docs = await db
       .collection("restaurants")
-      .find({
-        $or: [
-          { name: new RegExp(term, "i", "g") },
-          {
-            menu: {
-              $elemMatch: { $elemMatch: { name: new RegExp(term, "i", "g") } },
-            },
+      .aggregate([
+        {
+          $addFields: {
+            arrayMenu: { $objectToArray: "$menu" },
           },
-        ],
-      })
+        },
+        {
+          $match: {
+            $or: [
+              {
+                arrayMenu: {
+                  $elemMatch: {
+                    v: { $elemMatch: { name: new RegExp(term, "ig") } },
+                  },
+                },
+              },
+              {
+                name: new RegExp(term, "ig"),
+              },
+            ],
+          },
+        },
+        {
+          $unset: "arrayMenu",
+        },
+        {
+          $sort: {
+            "user_rating.aggregate_rating": -1,
+          },
+        },
+        {
+          $limit: 30,
+        },
+      ])
       .toArray();
+
     res.send({ success: true, docs });
   } catch (error) {
     console.log(error);
